0e75724603e4c9fb6fa778f70455a28a3df50f34
zxcvbn418s <dd.feyyeuuegs@gmail.com>
2026-06-30 14:34:37 +0330

    Update installed_app.dart

--- lib/models/installed_app.dart
+++ lib/models/installed_app.dart
@@ -1,1 +1,24 @@
+import 'dart:convert';
+import 'dart:typed_data';
 
+class InstalledApp {
+  final String name;
+  final String packageName;
+  final Uint8List iconBytes;
+
+  const InstalledApp({
+    required this.name,
+    required this.packageName,
+    required this.iconBytes,
+  });
+
+  factory InstalledApp.fromMap(Map<dynamic, dynamic> map) {
+    final iconBase64 = map['icon']?.toString() ?? '';
+
+    return InstalledApp(
+      name: map['name']?.toString() ?? 'Unknown',
+      packageName: map['packageName']?.toString() ?? '',
+      iconBytes: base64Decode(iconBase64),
+    );
+  }
+}
